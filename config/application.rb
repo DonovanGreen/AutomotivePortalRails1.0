da958d47a9cb54cae18e1512017ff1b47592b87d
DonovanGreen <racquel@dgtwo.com>
2017-12-12 09:41:49 -0500

    added rack cors

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'rack/cors'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -13,6 +14,12 @@ module RailsAutomotivePortal
 
     config.api_only = true
 
+    config.middleware.insert_before 0, Rack::Cors do
+          allow do
+            origins '*'
+            resource '*', :headers => :any, :methods => [:get, :post, :options]
+          end
+        end
 
   end
 end
