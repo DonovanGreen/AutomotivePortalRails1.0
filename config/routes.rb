7e4e2f25e479ab973afa5f1943e8c3b58cf7da20
DonovanGreen <racquel@dgtwo.com>
2017-12-12 07:39:33 -0500

    rack cors problems

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, :projects, :projectcategories, :comments
       post '/sessions', to: 'sessions#create'
-      get '/sessions/current_user', to: 'sessions#show'
+      post '/sessions/current_user', to: 'sessions#show'
       post '/users/get_clients', to: 'users#get_clients'
       post '/users/get_admins', to: 'users#get_admins'
       post '/users/get_client_project_categories', to: 'users#get_client_project_categories'
