c0d1e54c8e20cc2a7a4cc939d0fa72476af8f8d8
DonovanGreen <racquel@dgtwo.com>
2017-12-12 07:55:27 -0500

    rack cors taken out of config.ru

--- config/application.rb
+++ config/application.rb
@@ -14,11 +14,11 @@ module RailsAutomotivePortal
     config.api_only = true
 
     config.middleware.insert_before 0, Rack::Cors do
-            allow do
-           origins '*'
-           resource '*', :headers => :any, :methods => [:get, :post, :delete, :patch, :options]
-         end
-       end
+          allow do
+            origins '*'
+            resource '*', :headers => :any, :methods => [:get, :post, :options]
+          end
+        end
 
   end
 end
