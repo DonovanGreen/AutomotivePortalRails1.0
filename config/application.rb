36372e32b865b372080420f3f21a0ea60258e64e
DonovanGreen <racquel@dgtwo.com>
2017-12-12 09:34:38 -0500

    rack cors problems

--- config/application.rb
+++ config/application.rb
@@ -13,12 +13,6 @@ module RailsAutomotivePortal
 
     config.api_only = true
 
-    config.middleware.insert_before 0, Rack::Cors do
-          allow do
-            origins 'https://dg-automotive-portal.herokuapp.com/'
-            resource '*', :headers => :any, :methods => [:get, :post, :options]
-          end
-        end
 
   end
 end
